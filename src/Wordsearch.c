d3c1838ad7d2a7895bb86bff78450a809dae99a5
Ben Kelly <ben@w0hc.com>
2022-09-20 14:55:37 -0500

    Started adding helper functions to the program

--- src/Wordsearch.c
+++ src/Wordsearch.c
@@ -21,13 +21,19 @@
 #include <math.h>
 
 //Defines
+#define BLANK ' '
 #define DEBUG true
+#define MAXPUZZLESIZE 60
+#define MAXWORDS 50
+
 
 //Global Declarations
-
+char PUZZLE[MAXPUZZLESIZE][MAXPUZZLESIZE];
 
 //Function Declarations
-
+void printPuzzle(int size_x, int size_y);
+void initPuzzle(int size_x, int size_y);
+void fillPuzzle(int size_x, int size_y);
 
 //Main Function
 int main(int argc, char* argv[])
@@ -35,12 +41,69 @@ int main(int argc, char* argv[])
   //First n Foremost: Check arguments
   if(argc != 4){
 	fprintf(stderr, "\nError: Incorrect number of arguments! Expected 3 arguments, recieved: [%d]", argc - 1);
-	fprintf(stderr, "\n\nUsage: ./collatz [starting number]\n");
+	fprintf(stderr, "\n\nUsage: ./wordsearch [input file] [puzzle dimensions]\n");
     return 1;
   }
+
+  for(int i = 0; )
+
+  int wordCount, puzzleSize_X, puzzleSize_Y;
+  char* wordlist[MAXWORDS];
 
   
 
   return 0;
 }
 
+/* Helper function: printPuzzle
+ * Params: Dimensions of puzzle
+ * Returns: Prints the puzzle's current state to screen
+ */
+void printPuzzle(int size_x, int size_y)
+{
+  for(int i = 0; i < size_y; i++)
+  {
+    for(int j = 0; j < size_x; j++)
+    {
+      printf("%c ", PUZZLE[j][i]);
+    }
+    printf("\n");
+  }
+}
+
+/* Helper function: initPuzzle
+ * Parameters: size of puzzle (x & y)
+ * Returns: Puzzle filled with blanks
+ * This function will fill the puzzle with blanks regardless of what's already set 
+ */
+void initPuzzle(int size_x, int size_y)
+{
+  for(int i = 0; i < size_y; i++)
+  {
+    for(int j = 0; j < size_x; j++)
+    {
+      PUZZLE[j][i] = BLANK;
+    }
+  }
+}
+
+/* Helper function: fillPuzzle
+ * Parameters: size of puzzle (x & y)
+ * Returns: Puzzle filled with random letters
+ * This function will ignore any spaces that already have letters in them
+ */
+void fillPuzzle(int size_x, int size_y)
+{
+  char c;
+  for(int i = 0; i < size_y; i++)
+  {
+    for(int j = 0; j < size_x; j++)
+    {
+      if(PUZZLE[j][i] == BLANK)
+      {
+        c = 'A' + (rand()%26);
+        PUZZLE[j][i] = c;
+      }
+    }
+  }
+}
